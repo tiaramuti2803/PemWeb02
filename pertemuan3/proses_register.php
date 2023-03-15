59c06b5109f7548d8547e0caf4f5d02e7d22e5f6
tiaramuti2803 <mutiaranoorfadhilla28@gmail.com>
2023-03-15 23:11:35 +0700

    push tugas

--- pertemuan3/proses_register.php
+++ pertemuan3/proses_register.php
@@ -1,43 +1,61 @@
 <?php
-$nim = $_POST['nim'];
-$nama = $_POST['nama'];
-$prodi = $_POST['prodi'];
-$gender = $_POST['gender'];
-$skill = $_POST['skill'];
-$domisili = $_POST['domisili'];
-$email = $_POST['email'];
 
-function skor_skill($skills)
-{
-    $skill_list =[
-    'HTML' => 10,
-    'CSS' => 10,
-    'Java Script' => 20,
-    'RWD Bootstrap' => 20,
-    'PHP' => 30,
-    'Python' => 30,
-    'Java' => 50,
+use LDAP\Result;
+if (isset($_POST['submit'])){
+    $nim = $_POST['nim'];
+    $nama = $_POST['nama'];
+    $gender = $_POST['gender'];
+    $prodi = $_POST['prodi'];
+    $skills = $_POST['skill'];
+    $domisili = $_POST['domisili'];
+
+    function skor_skill($skills)
+    {
+    $skill_list = [
+        'HTML' => 10,
+        'CSS' => 10,
+        'JavaScript' => 20,
+        'RWD Bootstrap' => 30,
+        'PHP' => 30,
+        'Python' => 30,
+        'Java' => 50
     ];
     $result = 0;
 
-    foreach($skills as $skill){
+    foreach ($skills as $skill) {
         $result = $result + $skill_list[$skill];
     }
 
     return $result;
 }
+$hasil_fungsi1 = skor_skill($skills);
+function kategori_skill($number)
+{
+    $kategori_skill = "";
+    if ($number == 0) {
+        $kategori_skill = "Tidak memadai";
+    } else if ($number <= 40) {
+        $kategori_skill = "Kurang";
+    } else if ($number <= 60) {
+        $kategori_skill = "Cukup";
+    } else if ($number <= 100) {
+        $kategori_skill = "Baik";
+    } else {
+        $kategori_skill = "Sangat Baik";
+    }
+    return $kategori_skill = "Sangat Baik";
+}
 
+$hasil_fungsi2 = kategori_skill($hasil_fungsi1);
 echo "NIM: $nim";
 echo "<br> Nama: $nama";
-echo "<br> Program Studi: $prodi";
 echo "<br> Jenis Kelamin: $gender";
-echo "<br> Skill Programming: ";
-foreach($skill as $skil){
-    echo $skil . ", ";
+echo "<br> Program Studi: $prodi";
+echo "<br> Skill Programing: ";
+foreach ($skills as $skill) {
+    echo $skill . ", ";
 }
 echo "<br> Tempat Domisili: $domisili";
-echo "<br> Email: $email";
-echo "<br> Skor Skill: ". skor_skill($skill);
-echo "<br> Kategori Skill: ";
-
-?>
+echo "<br> Skor Skill: " . $hasil_fungsi1;
+echo "<br> Kategori Skill: " . $hasil_fungsi2;
+}
